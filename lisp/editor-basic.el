13d0a8a4873be8d1efd283ceba35e2ad8829e566
Vietor Liu <vietor.liu@gmail.com>
2024-06-21 15:51:49 +0800

    Add more config

--- lisp/editor-basic.el
+++ lisp/editor-basic.el
@@ -22,19 +22,34 @@
 (setq-default major-mode 'text-mode)
 (setq initial-major-mode 'text-mode)
 
-;; Use better editor behavior
-
-(setq-default column-number-mode t
+;; Use bettert behavior
+(setq-default case-fold-search t
+              column-number-mode t
               truncate-lines nil
               indicate-empty-lines t
-              sentence-end-double-space nil
-              shift-select-mode nil
-              case-fold-search t)
+              sentence-end-double-space nil)
 
+;; Use better selection
 (cua-selection-mode t)
-(add-hook 'after-init-hook 'show-paren-mode)
-(add-hook 'after-init-hook 'transient-mark-mode)
+(setq-default shift-select-mode nil)
 (add-hook 'after-init-hook 'delete-selection-mode)
+
+;; Use better mark operation
+(global-set-key (kbd "C-.") 'set-mark-command)
+(global-set-key (kbd "C-x C-.") 'pop-global-mark)
+(setq-default set-mark-command-repeat-pop t)
+(add-hook 'after-init-hook 'transient-mark-mode)
+
+;; Use better paren highlight
+(add-hook 'after-init-hook 'show-paren-mode)
+(when (fboundp 'electric-pair-mode)
+  (add-hook 'after-init-hook 'electric-pair-mode))
+
+;; Disable file lock & backup
+(setq create-lockfiles nil
+      make-backup-files nil
+      auto-save-default nil
+      auto-save-list-file-prefix nil)
 
 (provide 'editor-basic)
 ;;; editor-basic.el ends here
