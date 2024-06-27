4e1c3c77f23f0e70ec6e8517b2aef25a834bd9c0
Vietor Liu <vietor.liu@gmail.com>
2024-06-27 22:16:50 +0800

    Add more config

--- lisp/toolkit-treesit.el
+++ lisp/toolkit-treesit.el
@@ -6,36 +6,37 @@
 
 (use-package treesit
   :when (and (fboundp 'treesit-available-p)
-	     (treesit-available-p))
+	         (treesit-available-p))
   :init
+  (setq treesit-font-lock-level 4)
   (add-to-list 'treesit-extra-load-path
-	       (concat user-emacs-space-directory "tree-sitter"))
+	           (concat user-emacs-space-directory "tree-sitter"))
 
   (defconst my/treesit-lang-to-grammar '(("c++" . "cpp")
-					 ("js" . "javascript")))
+					                     ("js" . "javascript")))
 
   (defconst my/treesit-lang-to-extra '(("c++" . "c-or-c++")))
 
   (defun my/treesit-lang-p (lang)
     (treesit-language-available-p
      (intern (or (cdr (assoc-string lang my/treesit-lang-to-grammar))
-		 lang))))
+		         lang))))
 
   (defun my/treesit-lang-remap (lang)
     (let ((ts-mode (intern (concat lang "-ts-mode")))
           (non-ts-mode (intern (concat lang "-mode"))))
       (when (and (fboundp ts-mode)
-		 (fboundp non-ts-mode))
+		         (fboundp non-ts-mode))
         (add-to-list 'major-mode-remap-alist (cons non-ts-mode ts-mode)))))
 
-  (defun my/auto-configure-treesit()
+  (defun my/treesit-auto-configure()
     (dolist (lang my/treesit-active-langs)
       (when (my/treesit-lang-p lang)
-	(my/treesit-lang-remap lang)
-	(let ((extra-lang (cdr (assoc-string lang my/treesit-lang-to-extra))))
-	  (when extra-lang
-	    (my/treesit-lang-remap extra-lang))))))
-  (add-hook 'after-init-hook 'my/auto-configure-treesit))
+	    (my/treesit-lang-remap lang)
+	    (let ((extra-lang (cdr (assoc-string lang my/treesit-lang-to-extra))))
+	      (when extra-lang
+	        (my/treesit-lang-remap extra-lang))))))
+  (add-hook 'after-init-hook 'my/treesit-auto-configure))
 
 (provide 'toolkit-treesit)
 ;;; toolkit-treesit.el ends here
