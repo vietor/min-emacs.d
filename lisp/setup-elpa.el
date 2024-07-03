1473d23e71d040b23a07489aa5085afe1690f4d2
Vietor Liu <vietor.liu@gmail.com>
2024-07-03 13:53:12 +0800

    Modify elpa repository

--- lisp/setup-elpa.el
+++ lisp/setup-elpa.el
@@ -6,12 +6,7 @@
 
 ;; Initialize repository
 
-(dolist (archive '(("gnu"    . "http://mirrors.tuna.tsinghua.edu.cn/elpa/gnu/")
-                   ("nongnu" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/nongnu/")
-                   ("melpa"  . "http://mirrors.tuna.tsinghua.edu.cn/elpa/melpa/")))
-  (unless (assoc (car archive) package-archives)
-    (add-to-list 'package-archives archive t)))
-
+(add-to-list 'package-archives '( "melpa" . "https://melpa.org/packages/") t)
 (setq package-user-dir
       (expand-file-name (format "elpa-%s.%s" emacs-major-version emacs-minor-version)
                         user-emacs-directory))
